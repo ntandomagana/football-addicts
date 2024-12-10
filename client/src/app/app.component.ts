6df1f71c2845517296dc47c6a8e3f9598ca8818b
ntandomagana <ntandomagana@gmail.com>
2024-12-10 14:06:50 +0200

    fix the buttons on navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './Components/nav/nav.component';
+import { HomeHeroComponent } from './Components/home-hero/home-hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, NavComponent,HomeHeroComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
