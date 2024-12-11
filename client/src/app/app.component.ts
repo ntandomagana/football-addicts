345ab89d624d3f53bdcadf60696bb2e50b705703
ntandomagana <ntandomagana@gmail.com>
2024-12-11 14:57:07 +0200

    add routing

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './Components/nav/nav.component';
 import { HomeHeroComponent } from './Components/home-hero/home-hero.component';
+import { AboutComponent } from './Components/about/about.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavComponent,HomeHeroComponent],
+  imports: [RouterOutlet, NavComponent,HomeHeroComponent, AboutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
