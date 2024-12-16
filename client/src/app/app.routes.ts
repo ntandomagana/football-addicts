29a05ba2b9bf13c2e79020ff86edd11f10d209d6
Ntando Magana <ntandomagana@gmail.com>
2024-12-16 16:19:09 +0200

    fixed images

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'login', component: Login },
+    { path: 'register', component: Register }
