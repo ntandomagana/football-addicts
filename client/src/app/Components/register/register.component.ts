2c9785d324b0556e506f4d1635a3d8dd28bc7cde
Ntando Magana <ntandomagana@gmail.com>
2024-12-17 00:28:56 +0200

    fix the register page

--- client/src/app/Components/register/register.component.ts
+++ client/src/app/Components/register/register.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructore(private fb: FormBuilder){
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
 
 }
