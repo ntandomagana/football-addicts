2f621b56e3ae5ccf4103a78dbfe55264456f2690
Ntando Magana <ntandomagana@gmail.com>
2024-12-16 21:43:01 +0200

    fix the routing issue

--- client/src/app/Components/nav/nav.component.ts
+++ client/src/app/Components/nav/nav.component.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
