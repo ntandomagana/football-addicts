323a56b73f518f2f6086807b0e89a08b918b9fe6
Ntando Magana <ntandomagana@gmail.com>
2024-12-16 20:21:16 +0200

    fix the routing issue

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { RouterModule, Router } from '@angular/router';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
 
 export const routes: Routes = [
-    { path: 'login', component: Login },
-    { path: 'register', component: Register }
+
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent }
+]
