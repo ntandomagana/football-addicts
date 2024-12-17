4e5a79d5b679fd6a48b6b7e1e358e07fb8a4a3a8
Ntando Magana <ntandomagana@gmail.com>
2024-12-17 18:36:48 +0200

    fix the ngIf on both l&r pages

--- client/src/app/Components/register/register.component.ts
+++ client/src/app/Components/register/register.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -12,12 +12,47 @@ import { RouterLink } from '@angular/router';
 export class RegisterComponent {
   registerForm: FormGroup;
 
-  constructore(private fb: FormBuilder){
+  constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      username: ['', Validators.required],
+      name: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
-  }
+  };
 
+  /**
+   * onSubmit handles the form submission.
+   * It checks if the form is valid and then logs the registration data or alerts the user if the form is invalid.
+   */
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      alert('Registered successfully');
+      // this.loginService
+      //   .register(
+      //     this.registerForm.value.name,
+      //     this.registerForm.value.surname,
+      //     this.registerForm.value.phone,
+      //     this.registerForm.value.email,
+      //     this.registerForm.value.password,
+      //   )
+      //   .subscribe( {
+      //     next: (data: any) => {
+      //       console.log(data);
+      //       alert("User registered successfully")
+      //       this.router.navigate(['/login'])
+      //       .then(() => {
+      //         window.location.reload()
+      //       });
+      //     },
+      //     error: (error: any) => {
+      //       alert("User already registered")
+      //     }
+
+      //   });
+    } else {
+      alert('Form is invalid. Please check your inputs.');
+    }
+  }
 }
