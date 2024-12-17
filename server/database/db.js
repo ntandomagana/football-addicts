436704efa27ab1d10a70b2cd9813d03476a3c472
Ntando Magana <ntandomagana@gmail.com>
2024-12-17 23:54:41 +0200

    fix the server issue

---
+++ server/database/db.js
@@ -0,0 +1,24 @@
+
+import pkg from "pg";
+const { Pool } = pkg;
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+pool.connect()
+.then(() => {
+    console.log('Connected to the database');
+})
+.catch((err) => {
+    console.error('Error connecting to the database');   
+});
+
+export { pool };
