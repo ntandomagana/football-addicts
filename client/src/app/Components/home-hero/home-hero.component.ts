81a68ea04d536773e4f758df75797b47074ddd8a
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-12-24 11:13:54 +0200

    Home-Hero component has the approapriate child components

--- client/src/app/Components/home-hero/home-hero.component.ts
+++ client/src/app/Components/home-hero/home-hero.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { BelowPitchComponent } from '../below-pitch/below-pitch.component';
 import { PitchComponent } from '../pitch/pitch.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home-hero',
-  imports: [],
+  imports: [
+    AboutComponent,
+    PitchComponent,
+    BelowPitchComponent,
+  ],
   templateUrl: './home-hero.component.html',
   styleUrl: './home-hero.component.css'
 })
